21e4cb4dcc23e89f33b1dd1153617d2e919a5967
markus <markuszimmermann3897@gmail.com>
2026-04-07 12:28:43 -0300

    ja coloquei a implementação pra o python poder carregar as subpastas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import config
 from discord.ext import commands
 import discord 
+import traceback
+from pathlib import Path
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,12 +11,26 @@ intents.message_content = True
 class MeuBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=intents)
-    
+
     async def setup_hook(self):
-        await self.load_extension("cogs.ping")
+        cogs_dir = Path("cogs")
+
+        for file in cogs_dir.rglob("*.py"):
+            if file.name == '__init__.py':
+                continue
+
+            module_name = ".".join(file.with_suffix('').parts)
+
+            try:
+                await self.load_extension(module_name)
+                print(f"[+] Extensão carregada: {module_name}")
+            except Exception as e:
+                print(f"[-] Erro ao carregar a extensão {module_name}: {e}")
+                traceback.print_exc()
+                
         await self.tree.sync()
         print("comandos de barra sincronizados com sucesso")
-    
+
     async def on_ready(self):
         print(f'Bot online e logado como {self.user}')
 
