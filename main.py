7fbd78860f6c859f11c88dd4c7c9e6d814a8f2fd
markus <markuszimmermann3897@gmail.com>
2026-04-07 12:21:13 -0300

    add a intents pro bot poder mandar mensagem

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import config
 from discord.ext import commands
 import discord 
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 class MeuBot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix="!", intents=discord.Intents.default())
+        super().__init__(command_prefix="!", intents=intents)
     
     async def setup_hook(self):
         await self.load_extension("cogs.ping")
