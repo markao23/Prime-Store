868654a67fbe3ac12d55e6daa4017bc305cdddeb
markus <markuszimmermann3897@gmail.com>
2026-04-07 11:50:21 -0300

    ja ta criado o main mano agora se vc quiser criar o comando de ping eu crio os demais

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ class MeuBot(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix="!", intents=discord.Intents.default())
     
-    async def setup_hook(self)
+    async def setup_hook(self):
+        await self.load_extension("cogs.ping")
+        await self.tree.sync()
+        print("comandos de barra sincronizados com sucesso")
+    
+    async def on_ready(self):
+        print(f'Bot online e logado como {self.user}')
+
+bot = MeuBot()
+
+if __name__ == "__main__":
+    bot.run(config.DISCORD_TOKEN)
