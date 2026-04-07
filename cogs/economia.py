a33e5d2c9ea9ac64887adc4dddcc8d33a7d7ba10
markus <markuszimmermann3897@gmail.com>
2026-04-07 14:10:45 -0300

    concertei erro que foi feito nas linhas 20 16 12 e tbm os erros das linha 23 34

--- cogs/economia.py
+++ cogs/economia.py
@@ -9,18 +9,18 @@ class Econimia(commands.Cog):
         self.bot = bot
     
     def carregar(self):
-        if not os.path.exists("database/dadps.jyson"):
+        if not os.path.exists("database/dados.json"):
             with open("datbase/dadps.jyson", "w") as f:
                 json.dump({}, f)
         
-        with open("database/dados.jyson", "r") as f:
+        with open("database/dados.json", "r") as f:
             return json.load(f)
 
     def salvar(self, dados):
-        with open("database/dados.jyson", "w") as f:
+        with open("database/dados.json", "w") as f:
             json.dump(dados, f, indent=4)
     
-    @app_command.command()
+    @app_command.command(name="saldo", description="veja o seu saldo na conta do bot")
     async def saldo(self, ctx):
         dados = self.carregar()
         user = str(ctx.author.id)
@@ -31,7 +31,7 @@ class Econimia(commands.Cog):
 
         await ctx.send(f"{ctx.author.mention}, seu saldo é de {dados[user]} moedas.")
     
-    @app_command.command()
+    @app_command.command(name="add", description="Adiciona moedas a um usuário (Apenas Admins)")
     async def add(self, ctx, member: discord.member, valor: int):
        if ctx.author.guid_permissions.administrator:
             dados = self.carregar()
